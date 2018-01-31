33be15678cc8b31933c318820970610c7a187c55
TOMAS BRAZAS - STUDENT <G00349242@gmit.ie>
2018-01-31 17:11:25 +0000

    s

---
+++ functionsTS.ts
@@ -0,0 +1,28 @@
+let myArray:Array<String> = [];
+
+function addTask(task: String) : number
+{
+    myArray.push(task);
+    console.log("String has been added:" + task);
+    return myArray.length;
+}
+
+function listAllTasks() : void
+{
+    for (let item of myArray)
+    {
+        console.log(item);
+    }
+}
+
+function deleteTask(task: String) : number
+{
+    let index : number = myArray.indexOf(task);
+
+    if (index > -1)
+    {
+        myArray.splice(index, 1);
+    }
+    console.log("Task has been deleted:" + task);
+    return myArray.length;
+}
