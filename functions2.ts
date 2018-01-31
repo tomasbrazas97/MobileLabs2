5f38e2a5e09afba657af434da735f6fb923f9687
TOMAS BRAZAS - STUDENT <G00349242@gmit.ie>
2018-01-31 17:50:31 +0000

    s

---
+++ functions2.ts
@@ -0,0 +1,39 @@
+export interface todoInterface{
+    tasks: Array<string>;
+    addTask(x: string): number;
+    listAllTasks(): void;
+    deleteTask(x: string): number;
+}
+class Todo implements todoInterface{
+    constructor(){}
+    myArray:Array<String> = [];
+    
+     addTask(task: String) : number
+    {
+        this.myArray.push(task);
+        console.log("String has been added:" + task);
+        return this.myArray.length;
+    }
+    
+     listAllTasks() : void
+    {
+        for (let item of this.myArray)
+        {
+            console.log(item);
+        }
+    }
+    
+     deleteTask(task: String) : number
+    {
+        let index : number = this.myArray.indexOf(task);
+    
+        if (index > -1)
+        {
+            this.myArray.splice(index, 1);
+            console.log("Task has been deleted:" + task);
+        }
+      
+        return this.myArray.length;
+    }
+    }
+    
